7170c8a4c567acf4b052885ca47d49e05b9f47b9
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-20 17:38:32 -0700

    added to dos from final round code review

--- static/app.js
+++ static/app.js
@@ -1,22 +1,38 @@
-BASE_URL = "http://localhost:5000"
+"use strict";
+
+const BASE_URL = "http://localhost:5000"
 
 $("#cupcakeForm").on("submit", makeCupcake);
 
+// TODO: Add docstrings for all functions.
 async function makeCupcake(evt){
   // console.log("evt=", evt);
   evt.preventDefault();
+
   const flavor = $("#flavor").val()
-  const rating = $("#rating").val()
+  const rating = $("#rating").val() // TODO: This is returning a string value, not an integer! (Turn into int before return server)
   const size = $("#size").val()
   const image = $("#image").val()
-  await submitCupcake();
-}
 
-async function submitCupcake(){
   const response = await axios.post(
-    "/api/cupcakes", {data: {flavor:flavor, rating:rating,
-      size:size, image:image}})
+    `${BASE_URL}/api/cupcakes`,
+    data = {
+      flavor,
+      rating,
+      size,
+      image
+    });
   console.log("response in submitCupcake=", response);
+
+  // TODO: Check jQuery docs and use trigger method so we don't need to manually
+  // iterate through each one individually.
+
+  for (let field of $("#cupcakeForm")[0]) {
+    field.value = "";
+  }
+
+  makeCupcakeList(); // TODO: Append to list item rather than refreshing whole list
+  // after refactoring makeListItems and makeCupcakeList
 
 }
 
@@ -26,15 +42,20 @@ async function getCupcakes(){
   return response.data.cupcakes;
 }
 
-function makeListItems(cupcake){
-  return $("<li>").text(`${cupcake.flavor}, ${cupcake.image}, ${cupcake.rating},
-  ${cupcake.size}`)
+function makeListItems(cupcake){ // TODO: rename to getCupcakeHTML and refactor to return just the HTML back
+  const $list_item = $("<li>").text(`${cupcake.flavor}, ${cupcake.rating},
+  ${cupcake.size}`);
+
+  const $img = $("<img>").attr("src", cupcake.image).width('200px');
+
+  $list_item.append($img) // TODO: remove appending
 }
 
 async function makeCupcakeList(){
-  let cupcakesList = await getCupcakes()
+  $(".cupcakeList").empty();
+  let cupcakesList = await getCupcakes();
   for(let cupcake of cupcakesList){
-    console.log("cupcake in loop=", cupcake)
+    console.log("cupcake in loop=", cupcake);
     $(".cupcakeList").append(makeListItems(cupcake));
   }
 }
